6e66f9f86b021ae63dd9956a6a7c35918f6372e0
Jawsarn <christian_markowicz@hotmail.com>
2016-04-06 17:34:11 +0200

    Added basic networking with a player that shoots

---
+++ Assets/Health.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+    public int maxHealth = 100;
+
+    // Will cause the server to update clients when changed
+    [SyncVar (hook = "OnChangeHealth")] public int currentHealth = 100;
+
+    public bool destroyOnDeath = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TakeDamage(int amount)
+    {
+        // Only server will manage damage
+        if(!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = maxHealth;
+                RpcRespawn();
+            }
+        }
+    }
+
+    void OnChangeHealth(int health)
+    {
+        // If we want to update healthbar here
+        currentHealth = health;
+    }
+
+    // Only client want to reset position, since he got authority of the player itself
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        transform.position = Vector3.zero;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // If we're not local
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime* 150f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    // Needs to have Cmd infront to be recognized
+    [Command]
+    void CmdFire()
+    {
+        // Create bullet
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        // Add velocity
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6.0f;
+
+        // Spawn on clients
+        NetworkServer.Spawn(bullet);
+
+        // Destroy after two seconds
+        Destroy(bullet, 2.0f);
+
+    }
+}
