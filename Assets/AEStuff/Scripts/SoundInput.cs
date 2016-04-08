4c10c14e83ac957cc08bfab92aae075029a405cb
Jawsarn <christian_markowicz@hotmail.com>
2016-04-08 19:52:33 +0200

    Fixed frequency 0-1

--- Assets/AEStuff/Scripts/SoundInput.cs
+++ Assets/AEStuff/Scripts/SoundInput.cs
@@ -11,7 +11,6 @@ public class SoundInput : NetworkBehaviour
     float[] spectrum = new float[spectrumSize];
     [SyncVar (hook = "OnFrequencyChange")]float frequency = 0;
     public float realFrequency;
-    public int test = 0;
 
     // Use this for initialization
     void Start () {
@@ -29,7 +28,6 @@ public class SoundInput : NetworkBehaviour
 
         int microhponeSamples = Microphone.GetPosition("Built-in Microphone");
 
-        test = microhponeSamples;
         // We delay before we start the fft 30 ms
         if (!audio.isPlaying && microhponeSamples / 44100 > 0.030f)
         {
@@ -47,23 +45,15 @@ public class SoundInput : NetworkBehaviour
         {
             if (spectrum[i] > 0.001f && spectrum[i] > max)
             {
+                max = spectrum[i];
                 highestBand = i;
             }
         }
 
 
-        frequency = (float)highestBand * binSize; 
+        frequency = Mathf.Min((((float)highestBand * binSize)/1000.0f), 1.0f); 
+        //frequency = ((float)highestBand * binSize);
         realFrequency = frequency;
-
-        //int i = 1;
-        //while (i < spectrum.Length - 1)
-        //{
-        //    Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
-        //    Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-        //    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
-        //    Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.yellow);
-        //    i++;
-        //}
     }
 
     float GetCurrentFrequency()
