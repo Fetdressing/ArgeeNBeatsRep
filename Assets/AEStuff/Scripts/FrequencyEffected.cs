e71500c88e956e86170217387a0fb084e92bf8eb
Jawsarn <christian_markowicz@hotmail.com>
2016-04-08 23:41:54 +0200

    Added way to find players on server typ

---
+++ Assets/AEStuff/Scripts/FrequencyEffected.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FrequencyEffected : MonoBehaviour {
+
+    public bool DirectEffected = true;
+
+    List<Transform> waypoints;
+
+	// Use this for initialization
+	void Start () {
+        waypoints = new List<Transform>();
+
+        foreach (Transform Trans in transform)
+        {
+            waypoints.Add(Trans);
+            Debug.Log("Added Trans");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!DirectEffected)
+        {
+            return;
+        }
+
+        
+	}
+}
---
+++ Assets/AEStuff/Scripts/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerManager : MonoBehaviour {
+
+    static List<GameObject> playerList = new List<GameObject>();
+    public int numPlayers = 0;
+
+    void Start()
+    {
+        playerList.Clear();
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject p in players)
+        {
+            playerList.Add(p);
+        }
+    }
+
+    void OnPlayerConnected()
+    {
+        playerList.Clear();
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject p in players)
+        {
+            playerList.Add(p);
+        }
+    }
+
+    void OnPlayerDisconnected(NetworkPlayer player)
+    {
+        playerList.Clear();
+        Network.RemoveRPCs(player);
+        Network.DestroyPlayerObjects(player);
+
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject p in players)
+        {
+            playerList.Add(p);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+}
