e7fc30b26e58cba3f7d0e34f1c0af2547b074c57
Jawsarn <christian_markowicz@hotmail.com>
2016-04-12 16:44:32 +0200

    Fixed frequency effected script to be working better

--- Assets/AEStuff/Scripts/FrequencyEffected.cs
+++ Assets/AEStuff/Scripts/FrequencyEffected.cs
@@ -6,9 +6,10 @@ using UnityEngine.Networking;
 public class FrequencyEffected : MonoBehaviour {
 
     public bool DirectEffected = true;
+    public Transform RemoteTransform;
     public float range = 0;
 
-
+    Transform transToCheck;
     Transform objectTrans;
     Rigidbody objectRigid;
     List<Transform> waypoints;
@@ -41,15 +42,19 @@ public class FrequencyEffected : MonoBehaviour {
             Debug.LogError("Less then two waypoints on a frq effected object: " + waypoints.Count);
         }
 
-        foreach (Transform Trans in waypoints)
+        if (DirectEffected)
         {
-            Debug.Log(Trans.name);
+            transToCheck = transform;
+        }
+        else
+        {
+            transToCheck = RemoteTransform;
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (!NetworkServer.active || !DirectEffected)
+        if (!NetworkServer.active)
         {
             return;
         }
@@ -59,7 +64,7 @@ public class FrequencyEffected : MonoBehaviour {
         foreach(GameObject player in PlayerManager.playerList)
         {
             // Check if inside range
-            if (Vector3.Distance(player.transform.position, transform.position) < range)
+            if (Vector3.Distance(player.transform.position, transToCheck.position) < range)
             {
                 // Add their frequency
                 totalFreq += player.GetComponent<SoundInput>().GetCurrentFrequency();
@@ -77,26 +82,51 @@ public class FrequencyEffected : MonoBehaviour {
         // borde vara längden från start till obj, och längd från start till waypoint
         // om obj längd är större, så borde jag sätta den till nästa waypoint pos,
         // öka sedan nästa waypoint index
-
         float lengthToWaypoint1 = Vector3.Distance(objectTrans.position, waypoints[currWaypoint + 1].position);
         float lengthToWaypoint2 = Vector3.Distance(objectTrans.position, waypoints[currWaypoint].position);
         float lengthWaypoints = Vector3.Distance(waypoints[currWaypoint].position, waypoints[currWaypoint + 1].position);
 
-        if (lengthToWaypoint1 >= lengthWaypoints && Vector3.Dot(objectRigid.velocity.normalized, (waypoints[currWaypoint + 1].position - objectTrans.position).normalized) > 0.5f)
+        Vector3 objDir;
+        if (inFrequency > 0.1f)
         {
+            objDir = waypoints[currWaypoint + 1].position - objectTrans.position;
+        }
+        else
+        {
+            objDir = waypoints[currWaypoint].position - objectTrans.position;
+            inFrequency = 0.3f;
+        }
+
+        objectRigid.AddForce(objDir * inFrequency*Time.deltaTime* 100.0f);
+
+        if (objectRigid.velocity.magnitude > 10)
+        {
+            objectRigid.velocity = objectRigid.velocity.normalized * 10.0f;
+        }
+
+
+        if (lengthToWaypoint2 >= lengthWaypoints &&
+    Vector3.Dot(objectRigid.velocity.normalized, (waypoints[currWaypoint + 1].position - objectTrans.position).normalized) < -0.5f)
+        {
+            objectRigid.velocity = Vector3.zero;
+            objectTrans.position = waypoints[currWaypoint + 1].position;
+
+            currWaypoint++;
+            currWaypoint = Mathf.Min(currWaypoint, waypoints.Count - 2);
             
+            Debug.Log("Entered first " + currWaypoint);
         }
-        else if (lengthToWaypoint2 >= lengthWaypoints && Vector3.Dot(objectRigid.velocity.normalized, (waypoints[currWaypoint].position - objectTrans.position).normalized) > 0.5f)
+        else if (lengthToWaypoint1 >= lengthWaypoints &&
+            Vector3.Dot(objectRigid.velocity.normalized, (waypoints[currWaypoint].position - objectTrans.position).normalized) < -0.5f)
         {
+            objectRigid.velocity = Vector3.zero;
+            objectTrans.position = waypoints[currWaypoint].position;
 
+            currWaypoint--;
+            currWaypoint = Mathf.Max(currWaypoint, 0);
+
+            Debug.Log("Entered second" + currWaypoint);
         }
 
-
-        //objectRigid.AddForce(objDir * inFrequency* 100.0f);
-
-        //if (objectRigid.velocity.magnitude > 10)
-        //{
-        //    objectRigid.velocity = objectRigid.velocity.normalized * 10.0f;
-        //}
     }
 }
